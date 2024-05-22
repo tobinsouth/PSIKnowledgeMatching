604e720881d06d0b86c45bb9d2842edfa38767a9
tobinsouth <tobin.south@gmail.com>
2024-05-22 15:41:00 -0700

    Addig Shayla task

---
+++ tobin/benchmarking_retrieval.py
@@ -0,0 +1,9 @@
+# This file will aggregate the results of the retrieval benchmarking
+
+
+# @ Shayla task
+# 1. Load in corpus of queries and documents
+# 2. Allow us to choose how we change these documents (noise, round, nothing).
+# 3. Run the retrieval on the documents with the chosen changes.
+# 4. Save the results.
+
